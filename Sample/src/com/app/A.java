249feee503296c8ebb53f209e8d58b7443436045
Satya <satyat543@gmail.com>
2018-11-23 10:07:35 +0530

    First commit

---
+++ Sample/src/com/app/A.java
@@ -0,0 +1,6 @@
+package com.app;
+
+public interface A {
+	int x = 5;
+	void show();
+}
---
+++ Sample/src/com/app/Test.java
@@ -0,0 +1,17 @@
+package com.app;
+
+public class Test implements A{
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+		System.out.println("Show method");
+	}
+	public static void main(String[] args) {
+		System.out.println(A.x);
+		Test t = new Test();
+		t.show();
+		
+	}
+
+}
